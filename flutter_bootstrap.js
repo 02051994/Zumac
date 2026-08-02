8c31e932d0a2ea1b64e4d6eebd8ec947e85a5fbf
02051994 <118220540+02051994@users.noreply.github.com>
2026-08-01 22:44:05 -0500

    Mejoras móviles de Zumac Consultor y Creator

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2162271085" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1569868106" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
