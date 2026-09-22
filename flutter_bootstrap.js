4e274dc78565608dd294d092c9ae8736838af6d5
02051994 <02051994@users.noreply.github.com>
2026-09-21 22:22:34 -0500

    Deploy Zumac web 0.0.16+16

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1912013461" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "86276406" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
